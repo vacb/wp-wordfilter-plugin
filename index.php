e7ebfa9e56a1e5f2963a9439b6b8b285fcdc7acb
Victoria Blackburn <victoria@sixtwo.tech>
2021-08-16 13:36:56 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+  Plugin Name: Word Filter Plugin
+  Description: Replaces a list of words.
+  Version 1.0
+  Author: Victoria
+  Author URI: https://github.com/vacb
+*/
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
